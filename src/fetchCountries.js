fc0c0a9c426b6424a670a0767b744f23a979d6a4
NovichenkoYura <novichenko.y@gmail.com>
2022-06-08 19:25:51 +0300

    find some mistakes in fetch

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,11 +1,12 @@
-function fetchCountries (name) {
-    return fetch(`https://restcountries.com/v3.1/name/{name}?fields=name,capital,population,flags,languages`)
+export function fetchCountries (name) {
+    return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
         .then(response => {
             if (!response.ok) {
                 throw new Error(response.status);
             }
             return response.json();
+        })
+        .catch(error => {
+            console.log(error)
         });    
-} 
-
-export default {fetchCountries}
+}
